38079df385cab6ce45363a0af174f0ce47ba5d2e
kormonditamas2222 <t.kormond2006@gmail.com>
2026-03-04 08:21:35 +0100

    alap class

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class BingoJatekos
+    {
+        string nev;
+        string[,] kartya;
+
+        public BingoJatekos(string nev, string[,] kartya)
+        {
+            this.nev = nev;
+            this.kartya = kartya;
+        }
+    }
+}
