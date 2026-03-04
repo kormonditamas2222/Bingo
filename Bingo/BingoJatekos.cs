930a023b69022bd8f5ee7fbcb267bc714c53c0e9
kormonditamas2222 <t.kormond2006@gmail.com>
2026-03-04 09:14:25 +0100

    6. feladat

--- Bingo/BingoJatekos.cs
+++ Bingo/BingoJatekos.cs
@@ -31,5 +31,28 @@ namespace Bingo
                 }
             }
         }
+        public bool BingoEll()
+        {
+            string x = "X";
+            if (kartya[0, 0] == x && kartya[1, 0] == x && kartya[2, 0] == x && kartya[3, 0] == x && kartya[4, 0] == x ||
+                kartya[0, 1] == x && kartya[1, 1] == x && kartya[2, 1] == x && kartya[3, 1] == x && kartya[4, 1] == x ||
+                kartya[0, 2] == x && kartya[1, 2] == x && kartya[2, 2] == x && kartya[3, 2] == x && kartya[4, 2] == x ||
+                kartya[0, 3] == x && kartya[1, 3] == x && kartya[2, 3] == x && kartya[3, 3] == x && kartya[4, 3] == x ||
+                kartya[0, 4] == x && kartya[1, 4] == x && kartya[2, 4] == x && kartya[3, 4] == x && kartya[4, 4] == x ||
+                kartya[0, 0] == x && kartya[0, 1] == x && kartya[0, 2] == x && kartya[0, 3] == x && kartya[0, 4] == x ||
+                kartya[1, 0] == x && kartya[1, 1] == x && kartya[1, 2] == x && kartya[1, 3] == x && kartya[1, 4] == x ||
+                kartya[2, 0] == x && kartya[2, 1] == x && kartya[2, 2] == x && kartya[2, 3] == x && kartya[2, 4] == x ||
+                kartya[3, 0] == x && kartya[3, 1] == x && kartya[3, 2] == x && kartya[3, 3] == x && kartya[3, 4] == x ||
+                kartya[4, 0] == x && kartya[4, 1] == x && kartya[4, 2] == x && kartya[4, 3] == x && kartya[4, 4] == x ||
+                kartya[0, 0] == x && kartya[1, 1] == x && kartya[2, 2] == x && kartya[3, 3] == x && kartya[4, 4] == x ||
+                kartya[4, 0] == x && kartya[3, 1] == x && kartya[2, 2] == x && kartya[1, 3] == x && kartya[0, 4] == x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
